8b03ab9bac3e18ac6372687504e8d183f769f11f
JQiana <luo10@iu.edu>
2025-03-05 16:49:36 -0500

    update

---
+++ week8/window.js
@@ -0,0 +1,14 @@
+console.log("Windows 7 is still the only OS tp use");
+
+alert("Run away NOW!!!");
+
+function gerFavoritePeepFlavor(){
+    const flavor = prompt("What's your favorite peep flavor?", "Regular");
+    console.log("Favorite flavor;", flavor);
+}
+
+function isTyGettingFlyers(){
+const chooseToLeave = confirm("Are you going to get the flyers?");
+console.log("You are getting the flyers.",chooseToLeave);
+}
+
